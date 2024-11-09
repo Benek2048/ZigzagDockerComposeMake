23dbf68ee1aaa40954d8a535da22f8b9be92ec81
Benek2048 <benek2048@gmail.com>
2024-11-10 00:51:11 +0100

    Add Windows resources and build script
    Introduced Windows-specific resource files and a build script to compile them using mingw-w64. This includes checks for necessary tools and prompts to manage existing output files.

---
+++ internal/assets/windows/resource.h
@@ -0,0 +1,10 @@
+/*
+Important requirements:
+- File must end with two empty lines
+- File must use Windows line endings (CRLF)
+- File must be saved in ASCII or UTF-8 without BOM format
+*/
+
+#define IDI_ICON1 101
+#define RT_MANIFEST 24
+
